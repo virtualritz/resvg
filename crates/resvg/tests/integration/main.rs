7b0837a9e06192b3f043561c4d62fea7b49af365
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-08-31 19:51:05 +0200

    Improve the test harness (#954)

--- crates/resvg/tests/integration/main.rs
+++ crates/resvg/tests/integration/main.rs
@@ -2,7 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use once_cell::sync::Lazy;
-use rgb::{FromSlice, RGBA8};
+use png::{BitDepth, ColorType, Encoder};
+use rgb::{FromSlice, Rgba, RGBA8};
+use std::cmp::max;
+use std::fs::File;
+use std::io::BufWriter;
 use std::process::Command;
 use std::sync::Arc;
 use usvg::fontdb;
@@ -30,118 +34,11 @@ static GLOBAL_FONTDB: Lazy<Arc<fontdb::Database>> = Lazy::new(|| {
 });
 
 pub fn render(name: &str) -> usize {
-    let svg_path = format!("tests/{}.svg", name);
-    let png_path = format!("tests/{}.png", name);
-
-    let opt = usvg::Options {
-        resources_dir: Some(
-            std::path::PathBuf::from(&svg_path)
-                .parent()
-                .unwrap()
-                .to_owned(),
-        ),
-        fontdb: GLOBAL_FONTDB.clone(),
-        ..usvg::Options::default()
-    };
-
-    let tree = {
-        let svg_data = std::fs::read(&svg_path).unwrap();
-        usvg::Tree::from_data(&svg_data, &opt).unwrap()
-    };
-
-    let size = tree
-        .size()
-        .to_int_size()
-        .scale_to_width(IMAGE_SIZE)
-        .unwrap();
-    let mut pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
-    let render_ts = tiny_skia::Transform::from_scale(
-        size.width() as f32 / tree.size().width() as f32,
-        size.height() as f32 / tree.size().height() as f32,
-    );
-    resvg::render(&tree, render_ts, &mut pixmap.as_mut());
-
-    if option_env!("REPLACE").is_some() {
-        pixmap.save_png(&png_path).unwrap();
-        Command::new("oxipng")
-            .args(["-o".to_owned(), "6".to_owned(), "-Z".to_owned(), png_path])
-            .output()
-            .unwrap();
-        panic!("new reference image created");
-    }
-
-    let mut rgba = pixmap.take();
-    demultiply_alpha(rgba.as_mut_slice().as_rgba_mut());
-
-    let expected_data = load_png(&png_path);
-    assert_eq!(expected_data.len(), rgba.len());
-
-    let mut pixels_d = 0;
-    for (a, b) in expected_data
-        .as_slice()
-        .as_rgba()
-        .iter()
-        .zip(rgba.as_rgba())
-    {
-        if is_pix_diff(*a, *b) {
-            pixels_d += 1;
-        }
-    }
-
-    // Save diff if needed.
-    // if pixels_d != 0 {
-    //     gen_diff(&name, &expected_data, rgba.as_slice()).unwrap();
-    // }
-
-    pixels_d
+    render_inner(name, TestMode::Normal)
 }
 
 pub fn render_extra_with_scale(name: &str, scale: f32) -> usize {
-    let svg_path = format!("tests/{}.svg", name);
-    let png_path = format!("tests/{}.png", name);
-
-    let opt = usvg::Options {
-        fontdb: GLOBAL_FONTDB.clone(),
-        ..usvg::Options::default()
-    };
-
-    let tree = {
-        let svg_data = std::fs::read(&svg_path).unwrap();
-        usvg::Tree::from_data(&svg_data, &opt).unwrap()
-    };
-
-    let size = tree.size().to_int_size().scale_by(scale).unwrap();
-    let mut pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
-
-    let render_ts = tiny_skia::Transform::from_scale(scale, scale);
-    resvg::render(&tree, render_ts, &mut pixmap.as_mut());
-
-    // pixmap.save_png(&format!("tests/{}.png", name)).unwrap();
-
-    let mut rgba = pixmap.take();
-    demultiply_alpha(rgba.as_mut_slice().as_rgba_mut());
-
-    let expected_data = load_png(&png_path);
-    assert_eq!(expected_data.len(), rgba.len());
-
-    let mut pixels_d = 0;
-    for (a, b) in expected_data
-        .as_slice()
-        .as_rgba()
-        .iter()
-        .zip(rgba.as_rgba())
-    {
-        if is_pix_diff(*a, *b) {
-            pixels_d += 1;
-        }
-    }
-
-    // Save diff if needed.
-    // if pixels_d != 0 {
-    //     gen_diff(&name, &expected_data, rgba.as_slice()).unwrap();
-    // }
-
-    pixels_d
+    render_inner(name, TestMode::Extra(scale))
 }
 
 pub fn render_extra(name: &str) -> usize {
@@ -149,11 +46,22 @@ pub fn render_extra(name: &str) -> usize {
 }
 
 pub fn render_node(name: &str, id: &str) -> usize {
+    render_inner(name, TestMode::Node(id))
+}
+
+pub fn render_inner(name: &str, test_mode: TestMode) -> usize {
     let svg_path = format!("tests/{}.svg", name);
     let png_path = format!("tests/{}.png", name);
+    let make_ref = std::env::var("MAKE_REF").is_ok();
 
     let opt = usvg::Options {
         fontdb: GLOBAL_FONTDB.clone(),
+        resources_dir: Some(
+            std::path::PathBuf::from(&svg_path)
+                .parent()
+                .unwrap()
+                .to_owned(),
+        ),
         ..usvg::Options::default()
     };
 
@@ -162,48 +70,169 @@ pub fn render_node(name: &str, id: &str) -> usize {
         usvg::Tree::from_data(&svg_data, &opt).unwrap()
     };
 
-    let node = tree.node_by_id(id).unwrap();
-    let size = node.abs_layer_bounding_box().unwrap().size().to_int_size();
-    let mut pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
-    resvg::render_node(node, tiny_skia::Transform::identity(), &mut pixmap.as_mut());
+    let size;
+    let mut pixmap;
 
-    // pixmap.save_png(&format!("tests/{}.png", name)).unwrap();
-
-    let mut rgba = pixmap.take();
-    demultiply_alpha(rgba.as_mut_slice().as_rgba_mut());
-
-    let expected_data = load_png(&png_path);
-    assert_eq!(expected_data.len(), rgba.len());
-
-    let mut pixels_d = 0;
-    for (a, b) in expected_data
-        .as_slice()
-        .as_rgba()
-        .iter()
-        .zip(rgba.as_rgba())
-    {
-        if is_pix_diff(*a, *b) {
-            pixels_d += 1;
+    match test_mode {
+        TestMode::Normal => {
+            size = tree
+                .size()
+                .to_int_size()
+                .scale_to_width(IMAGE_SIZE)
+                .unwrap();
+            pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
+            let render_ts = tiny_skia::Transform::from_scale(
+                size.width() as f32 / tree.size().width() as f32,
+                size.height() as f32 / tree.size().height() as f32,
+            );
+            resvg::render(&tree, render_ts, &mut pixmap.as_mut());
+        }
+        TestMode::Node(id) => {
+            let node = tree.node_by_id(id).unwrap();
+            size = node.abs_layer_bounding_box().unwrap().size().to_int_size();
+            pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
+            resvg::render_node(node, tiny_skia::Transform::identity(), &mut pixmap.as_mut());
+        }
+        TestMode::Extra(scale) => {
+            size = tree.size().to_int_size().scale_by(scale).unwrap();
+            pixmap = tiny_skia::Pixmap::new(size.width(), size.height()).unwrap();
+            let render_ts = tiny_skia::Transform::from_scale(scale, scale);
+            resvg::render(&tree, render_ts, &mut pixmap.as_mut());
         }
     }
 
-    // Save diff if needed.
-    // if pixels_d != 0 {
-    //     gen_diff(&name, &expected_data, rgba.as_slice()).unwrap();
-    // }
+    let actual_image = {
+        let (width, height) = (pixmap.width(), pixmap.height());
+        let mut data = pixmap.clone().take();
+        demultiply_alpha(data.as_mut_slice().as_rgba_mut());
 
-    pixels_d
+        TestImage::new_with(data, width, height)
+    };
+
+    let make_ref_fn = || -> ! {
+        pixmap.save_png(&png_path).unwrap();
+        Command::new("oxipng")
+            .args([
+                "-o".to_owned(),
+                "6".to_owned(),
+                "-Z".to_owned(),
+                png_path.clone(),
+            ])
+            .output()
+            .unwrap();
+        panic!("new reference image created");
+    };
+
+    let reference_image = if let Ok(image_data) = std::fs::read(&png_path) {
+        load_png(image_data)
+    } else {
+        if make_ref {
+            make_ref_fn();
+        } else {
+            panic!("missing reference image");
+        }
+    };
+
+    if let Some((diff_image, pixel_diff)) = get_diff(&reference_image, &actual_image) {
+        if make_ref {
+            make_ref_fn();
+        } else {
+            let _ = std::fs::create_dir_all("tests/diffs");
+            diff_image.save_png(&format!("tests/diffs/{}.png", name.replace("/", "_")));
+
+            pixel_diff
+        }
+    } else {
+        0
+    }
 }
 
-fn load_png(path: &str) -> Vec<u8> {
-    let data = std::fs::read(path).unwrap();
+/// Returns `Some` if there is at least one different pixel, and `None` if the images match.
+fn get_diff(expected_image: &TestImage, actual_image: &TestImage) -> Option<(TestImage, usize)> {
+    const DIFF_THRESHOLD: u8 = 1;
+
+    let width = max(expected_image.width, actual_image.width);
+    let height = max(expected_image.height, actual_image.height);
+
+    let mut diff_image = TestImage::new(3 * width, height);
+
+    let mut pixel_diff = 0;
+
+    for x in 0..width {
+        for y in 0..height {
+            let actual_pixel = actual_image.get_pixel(x, y);
+            let expected_pixel = expected_image.get_pixel(x, y);
+
+            match (actual_pixel, expected_pixel) {
+                (Some(actual), Some(expected)) => {
+                    diff_image.set_pixel(x, y, expected);
+                    diff_image.set_pixel(x + 2 * width, y, actual);
+                    if is_pix_diff(&expected, &actual, DIFF_THRESHOLD) {
+                        pixel_diff += 1;
+                        diff_image.set_pixel(x + width, y, Rgba::new(255, 0, 0, 255));
+                    } else {
+                        diff_image.set_pixel(x + width, y, Rgba::new(0, 0, 0, 255));
+                    }
+                }
+                (Some(actual), None) => {
+                    pixel_diff += 1;
+                    diff_image.set_pixel(x + 2 * width, y, actual);
+                    diff_image.set_pixel(x + width, y, Rgba::new(255, 0, 0, 255));
+                }
+                (None, Some(expected)) => {
+                    pixel_diff += 1;
+                    diff_image.set_pixel(x, y, expected);
+                    diff_image.set_pixel(x + width, y, Rgba::new(255, 0, 0, 255));
+                }
+                _ => {
+                    pixel_diff += 1;
+                    diff_image.set_pixel(x, y, Rgba::new(255, 0, 0, 255));
+                    diff_image.set_pixel(x + width, y, Rgba::new(255, 0, 0, 255));
+                }
+            }
+        }
+    }
+
+    if pixel_diff > 0 {
+        Some((diff_image, pixel_diff))
+    } else {
+        None
+    }
+}
+
+/// Demultiplies provided pixels alpha.
+fn demultiply_alpha(data: &mut [RGBA8]) {
+    for p in data {
+        let a = p.a as f64 / 255.0;
+        p.b = (p.b as f64 / a + 0.5) as u8;
+        p.g = (p.g as f64 / a + 0.5) as u8;
+        p.r = (p.r as f64 / a + 0.5) as u8;
+    }
+}
+
+fn is_pix_diff(pixel1: &Rgba<u8>, pixel2: &Rgba<u8>, threshold: u8) -> bool {
+    if pixel1.a == 0 && pixel2.a == 0 {
+        return false;
+    }
+
+    let mut different = false;
+
+    different |= pixel1.r.abs_diff(pixel2.r) > threshold;
+    different |= pixel1.g.abs_diff(pixel2.g) > threshold;
+    different |= pixel1.b.abs_diff(pixel2.b) > threshold;
+    different |= pixel1.a.abs_diff(pixel2.a) > threshold;
+
+    different
+}
+
+fn load_png(data: Vec<u8>) -> TestImage {
     let mut decoder = png::Decoder::new(data.as_slice());
     decoder.set_transformations(png::Transformations::normalize_to_color8());
     let mut reader = decoder.read_info().unwrap();
     let mut img_data = vec![0; reader.output_buffer_size()];
     let info = reader.next_frame(&mut img_data).unwrap();
 
-    match info.color_type {
+    let data = match info.color_type {
         png::ColorType::Rgb => {
             panic!("RGB PNG is not supported.");
         }
@@ -235,53 +264,72 @@ fn load_png(path: &str) -> Vec<u8> {
         png::ColorType::Indexed => {
             panic!("Indexed PNG is not supported.");
         }
-    }
+    };
+
+    TestImage::new_with(data, info.width, info.height)
 }
 
-// TODO: remove
-fn is_pix_diff(c1: rgb::RGBA8, c2: rgb::RGBA8) -> bool {
-    (c1.r as i32 - c2.r as i32).abs() > 1
-        || (c1.g as i32 - c2.g as i32).abs() > 1
-        || (c1.b as i32 - c2.b as i32).abs() > 1
-        || (c1.a as i32 - c2.a as i32).abs() > 1
+struct TestImage {
+    data: Vec<u8>,
+    width: u32,
+    height: u32,
 }
 
-#[allow(dead_code)]
-fn gen_diff(name: &str, img1: &[u8], img2: &[u8]) -> Result<(), png::EncodingError> {
-    assert_eq!(img1.len(), img2.len());
-
-    let mut img3 = Vec::with_capacity((img1.len() as f32 * 0.75).round() as usize);
-    for (a, b) in img1.as_rgba().iter().zip(img2.as_rgba()) {
-        if is_pix_diff(*a, *b) {
-            img3.push(255);
-            img3.push(0);
-            img3.push(0);
-        } else {
-            img3.push(255);
-            img3.push(255);
-            img3.push(255);
+impl TestImage {
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            data: vec![0; width as usize * height as usize * 4],
+            width,
+            height,
         }
     }
 
-    let path = std::path::PathBuf::from(format!("tests/{}-diff.png", name));
-    let file = std::fs::File::create(path)?;
-    let ref mut w = std::io::BufWriter::new(file);
+    fn new_with(data: Vec<u8>, width: u32, height: u32) -> Self {
+        Self {
+            data,
+            width,
+            height,
+        }
+    }
 
-    let mut encoder = png::Encoder::new(w, IMAGE_SIZE, IMAGE_SIZE);
-    encoder.set_color(png::ColorType::Rgb);
-    encoder.set_depth(png::BitDepth::Eight);
-    let mut writer = encoder.write_header()?;
-    writer.write_image_data(&img3)
+    fn get_pixel(&self, x: u32, y: u32) -> Option<Rgba<u8>> {
+        if x >= self.width || y >= self.height {
+            return None;
+        }
+
+        let pos = self.width as usize * (y as usize) + x as usize;
+
+        Some(self.data.as_rgba()[pos])
+    }
+
+    fn set_pixel(&mut self, x: u32, y: u32, val: Rgba<u8>) {
+        let pos = self.width as usize * (y as usize) + x as usize;
+
+        self.data.as_rgba_mut()[pos] = val;
+    }
+
+    fn save_png(&self, path: &str) {
+        let file = File::create(path).unwrap();
+        let ref mut w = BufWriter::new(file);
+
+        let mut encoder = Encoder::new(w, self.width, self.height);
+        encoder.set_color(ColorType::Rgba);
+        encoder.set_depth(BitDepth::Eight);
+
+        let mut writer = encoder.write_header().unwrap();
+        writer.write_image_data(&self.data).unwrap();
+        writer.finish().unwrap();
+    }
 }
 
-/// Demultiplies provided pixels alpha.
-fn demultiply_alpha(data: &mut [RGBA8]) {
-    for p in data {
-        let a = p.a as f64 / 255.0;
-        p.b = (p.b as f64 / a + 0.5) as u8;
-        p.g = (p.g as f64 / a + 0.5) as u8;
-        p.r = (p.r as f64 / a + 0.5) as u8;
-    }
+#[derive(Copy, Clone)]
+pub enum TestMode<'a> {
+    /// Render a node by its ID.
+    Node(&'a str),
+    /// Render an `extra` test with a specific scale.
+    Extra(f32),
+    /// Render a normal SVG test.
+    Normal,
 }
 
 /// A simple stderr logger.
